484158ab537383b3e5f64e9bfa788fa06e247d41
Jeith <keithp1208@yahoo.com>
2022-06-26 03:54:47 -0500

    feat: added service worker and pwa optimizations

---
+++ images/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+    // registering service worker...
+  })
+  
+  self.addEventListener('fetch', function(event) {
+    // intercepting requests to check for the file or data in the cache
+  })
+  
+  self.addEventListener('activate', function(event) {
+    // service worker activated!
+  })
